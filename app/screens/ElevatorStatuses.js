733acdb8d8f7c7ef657a73a2e9def75e6d8c3c02
Jacob Gomez <jakegomez814@hotmail.com>
2022-01-15 15:01:22 -0500

    half

---
+++ app/screens/ElevatorStatuses.js
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from "react";
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import colors from "../config/colors.js";
+
+const ElevatorStatuses = (props) => {
+  const { id } = props.route.params;
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  
+  useEffect(() => {
+    fetch("jakerocket.azurewebsites.net/elevator/" + id)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, [data]);
+
+  useEffect(() => {
+    return () => {
+      console.log("cleans useEffect");
+    };
+  }, [isLoading]);
+
+  function updateStatus() {
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Accept", "application/json");
+    headers.append("Authorization", "Basic ");
+    headers.append("Origin", "*");
+
+    fetch("jakerocket.azurewebsites.net/elevator/" + id, {
+      method: "GET",
+      mode: "cors",
+      headers: new Headers({
+        "Content-Type": " application/json", 
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT",
+        "Access-Control-Allow-Headers": "Origin, Content-Type, Accept",
+      }),
+      body: JSON.stringify({
+        id: id,
+        status: "Active",
+      }),
+    })
+      .then((response) => response.text())
+      .then((responseText) => {
+        alert(responseText);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  return (
+    <View>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <Text
+          style={[
+            styles.status,
+            data.status == "Inactive"
+              ? { backgroundColor: "red" }
+              : { backgroundColor: "green" },
+          ]}
+        >
+          {data.status}
+        </Text>
+      )}
+      <TouchableOpacity>
+        <Button
+          style={styles.buttonText}
+          mode="outlined"
+          onPress={() => updateStatus()}
+          title="Set As Active"
+        ></Button>
+        <Button
+          style={styles.buttonText}
+          style={styles.backButton}
+          onPress={() => props.navigation.navigate("Home")}
+          title="Return To List"
+        ></Button>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  instructions: {
+    color: "#888",
+    fontSize: 18,
+    margin: 15,
+  },
+  status: {
+    padding: 20,
+    borderRadius: 5,
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  textinput: {
+    fontSize: 30,
+    height: 40,
+    margin: 10,
+  },
+  elevatorId: {
+    backgroundColor: "red",
+    padding: 20,
+    borderRadius: 5,
+    textAlign: "center",
+  },
+  elevatorIdText: {
+    fontSize: 20,
+    color: "#fff",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  backButton: {
+    backgroundColor: colors.primary,
+    margin: 15,
+    padding: 5,
+  },
+});
+
+export default ElevatorStatuses;
---
+++ app/screens/LogIn.js
@@ -0,0 +1,96 @@
+// import { appendFile } from "fs";
+import React from "react";
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Alert,
+  Text,
+  useState,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import colors from "../config/colors.js";
+
+function LogIn(props) {
+  const [email, onChangeText] = React.useState("");
+
+  const authenticate = () => {
+    fetch("https://jakerocket.azurewebsites.net/employee/valid/" + email)
+      .then((result) => result.json())
+      .then((resultJson) => {
+        if (resultJson == true) {
+          props.navigation.navigate("Home");
+          console.log(authenticate);
+        } else {
+          Alert.alert("Enter employee email");
+        }
+      });
+  };
+
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/images/AdobeStock_33298664.jpeg")}
+    >
+      <View style={styles.Seperator}>
+        <View style={styles.LogInContainer}>
+          <TextInput
+            placeholder="Enter your email"
+            onChangeText={(text) => onChangeText(text)}
+            value={email}
+          />
+        </View>
+        <TouchableOpacity>
+          <Button
+            style={styles.LogInButton}
+            onPress={authenticate}
+            title="Sign-In"
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/images/R2.png")}
+        />
+      </View>
+    </ImageBackground>
+  );
+}
+
+export default LogIn;
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: 290,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: "12%",
+    alignItems: "center",
+  },
+  Seperator: {
+    display: "flex",
+    flexDirection: "row-reverse",
+    justifyContent: "space-between",
+  },
+  LogInContainer: {
+    flex: 1.0,
+    width: 250,
+    bottom: "0%",
+    backgroundColor: "rgba(52, 52, 52, 0.49)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
